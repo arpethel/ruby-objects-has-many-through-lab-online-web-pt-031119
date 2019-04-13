5681e5f1f69934fc41927c173688c656c52f827a
Ryan Pethel <arpethel@gmail.com>
2019-04-13 22:44:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,7 @@ class Artist
   end
 
   def new_song(name, genre)
-    binding.pry
+    # binding.pry
     song = Song.new(name, self, genre)
     # the song should know it belongs to the artist
   end
