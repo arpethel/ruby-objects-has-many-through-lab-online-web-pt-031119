745e6091575e960dd2ab33b4b1d5be24caa56a5c
Ryan Pethel <arpethel@gmail.com>
2019-04-13 21:51:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,14 @@
 require 'pry'
 
 class Song
-
   attr_accessor :name, :artist, :genre
-
   @@all = []
-
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@all << self
-    binding.pry
+    # binding.pry
   end
 
   def self.all
