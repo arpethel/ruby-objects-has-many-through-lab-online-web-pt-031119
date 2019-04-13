d9ec047738992124bb77c4048d987923cb7d7161
Ryan Pethel <arpethel@gmail.com>
2019-04-13 22:37:10 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,49 +1,34 @@
-# class Customer
-#   attr_accessor :name, :age
-#
-#   @@all = []
-#
-#   def initialize(name, age)
-#     @name = name
-#     @age = age
-#     @@all << self
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def meals
-#     Meal.all.select do |meal|
-#       meal.customer == self
-#     end
-#   end
-#
-#   def waiters
-#     meals.map do |meal|
-#       meal.waiter
-#     end
-#   end
-#
-#   def new_meal(waiter, total, tip=0)
-#     Meal.new(waiter, self, total, tip)
-#   end
-#
-#   def new_meal_20_percent(waiter, total)
-#     tip = total * 0.2
-#     Meal.new(waiter, self, total, tip)
-#   end
-#
-#   def self.oldest_customer
-#     oldest_age = 0
-#     oldest_customer = nil
-#     self.all.each do |customer|
-#       if customer.age > oldest_age
-#         oldest_age = customer.age
-#         oldest_customer = customer
-#       end
-#     end
-#     oldest_customer
-#   end
-#
-# end
+require 'pry'
+
+class Genre
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each {|genre| puts genre}
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    @@all.map do |genre|
+      genre.artist
+    end
+  end
+
+
+
+
+
+end
