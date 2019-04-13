5f34bed0c135e450601b4db2b27560dfda5ff51f
Ryan Pethel <arpethel@gmail.com>
2019-04-13 22:21:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,54 +1,22 @@
 require 'pry'
 
 class Artist
-  class Customer
-    attr_accessor :name, :age
+  attr_accessor :name
 
-    @@all = []
+  @@all = []
 
-    def initialize(name, age)
-      @name = name
-      @age = age
-      @@all << self
-    end
-
-    def self.all
-      @@all
-    end
-
-    def meals
-      Meal.all.select do |meal|
-        meal.customer == self
-      end
-    end
-
-    def waiters
-      meals.map do |meal|
-        meal.waiter
-      end
-    end
-
-    def new_meal(waiter, total, tip=0)
-      Meal.new(waiter, self, total, tip)
-    end
-
-    def new_meal_20_percent(waiter, total)
-      tip = total * 0.2
-      Meal.new(waiter, self, total, tip)
-    end
-
-    def self.oldest_customer
-      oldest_age = 0
-      oldest_customer = nil
-      self.all.each do |customer|
-        if customer.age > oldest_age
-          oldest_age = customer.age
-          oldest_customer = customer
-        end
-      end
-      oldest_customer
-    end
-
+  def initialize(name)
+    @name = name
+    @age = age
+    @@all << self
   end
 
+  def self.all
+    @@all.each {|song| puts song}
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+    # the song should know it belongs to the artist
+  end
 end
